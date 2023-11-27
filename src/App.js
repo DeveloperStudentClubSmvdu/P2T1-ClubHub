dc21e3a1fdc2ca1ce85c3e10e012052c497fa65b
SuyashaAndotra <145702198+SuyashaAndotra@users.noreply.github.com>
2023-11-27 15:27:26 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,42 +1,14 @@
 import React from "react";
-import ClubCards from './Components/clubCard';
-import EventCardForm from './Components/Events';
-import EventCard from './Components/EventCard'
-import Feed from './Components/Feed'
-import NavBar from "./Components/Navbar";
-import Footer from './Components/Footer';
-import HeroSection from "./Components/Hero"
 import './App.css';
+import Login from "../Container/Login"; 
+import Home from "../Container/ Home";
 
 function MyApp() {
-  return (
-    <div>
-
-      <NavBar />
-
-      <HeroSection />
-
-      <section className="contain">
-        <h2>Our Clubs</h2>
-        <ClubCards />
-      </section>
-
-      <section className="event-form">
-        <EventCardForm />
-      </section>
-
-      <section className="contain">
-        <EventCard />
-      </section>
-
-      <Feed />
-
-
-      <Footer />
-      
-
+  return ( 
+    <div> 
+     <Home/>
     </div>
-  );
+    );
 }
 
 export default MyApp;
